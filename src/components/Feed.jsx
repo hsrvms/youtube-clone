c79c42288ad90f294d0b9f7dafe42523b71f23e7
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-01-25 13:19:44 +0300

    Feat add feed and sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { SideBar, Videos } from './';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
 
-export default Feed
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright 2023 BlaBla
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          New <span style={{ color: '#f31503' }}>videos</span>
+        </Typography>
+        <Videos 
+          videos={[]}
+        />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
